95881b0923cf64a30cccfe10d50e6ab422ffffec
marilia.neves <marilia.neves@dextra-sw.com>
2022-02-17 12:31:53 -0300

    adição de teste de conexão com backend

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>  
+ <cidades />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cidades from "./components/Cidades";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Cidades
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Cidades.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">    
+    <ul>
+      <li v-for="item in cidadeList" :key="item.nome">
+        {{ item.id }}
+        {{ item.nome }}
+        {{ item.estado }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cidadeList: [],
+    };
+  },
+   mounted: function () {
+    this.reverseMessage();
+  },
+  methods: {
+    async reverseMessage() {
+      const gestao = "http://127.0.0.1:8000/cidade/";
+      const response = await fetch(gestao);
+      this.cidadeList = await response.json();
+    },
+  },
+};
+
+ 
+</script>
